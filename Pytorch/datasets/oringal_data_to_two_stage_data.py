90da9b231bb4ec4ee68ead22c5a81bf7a5380e49
HUSTHY <840499869@qq.com>
2020-09-09 23:44:13 +0800

    process data to two stage

--- Pytorch/datasets/oringal_data_to_two_stage_data.py
+++ Pytorch/datasets/oringal_data_to_two_stage_data.py
@@ -1,21 +1,56 @@
 import json
 import os
 
-def get_text_bio_attri(file_path):
+
+def get_text_bio_attri(file_path,save_path):
     with open(file_path,'r') as f:
         lines = f.readlines()
         for line in lines:
             line = json.loads(line)
             text = line.get('text','')
             label = line.get('label','')
+            bios = get_bios(text,label)
+            attris = get_attris(text,label)
+
+
+def get_bios(text,label):
+    """ 根据文本和标签，把对应的每个字的BIO标签找到 """
+    bios = ['O']* len(text)
+    index_start_and_ends = []
+    for _,entitiys in label.items():
+        for _,v in entitiys.items():
+            index_start_and_ends.extend(v)
+    for start_end in index_start_and_ends:
+        start = int(start_end[0])
+        end = int(start_end[1])
+        bios[start] = 'B'
+        for i in range(start+1,end+1):
+            bios[i] = 'I'
+    return bios
+
+
+def get_attris(text,label):
+    attris = ['NULL'] * len(text)
+    index_start_and_ends = []
+    for _,entitiys in label.items():
+        for _,v in entitiys.items():
+            index_start_and_ends.extend(v)
+    for start_end in index_start_and_ends:
+        start = int(start_end[0])
+        end = int(start_end[1])
+    return attris
+
+
+
 
 
 def main():
-    dir_path = 'datasets/oringal_data'
+    dir_path = 'oringal_data'
     files = os.listdir(dir_path)
     for file in files:
         file_path = os.path.join(dir_path, file)
-        get_text_bio_attri(file_path)
+        save_path = os.path.join('two_stage_data',file.split('.')[0])
+        get_text_bio_attri(file_path,save_path)
 
 if __name__ == '__main__':
     main()
