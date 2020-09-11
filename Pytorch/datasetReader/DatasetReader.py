39b3f6b607c4699830507d95978a3497e8c609a6
huangyang <840499869@qq.com>
2020-09-11 18:38:29 +0800

    modify datasetreader code

--- Pytorch/datasetReader/DatasetReader.py
+++ Pytorch/datasetReader/DatasetReader.py
@@ -21,23 +21,23 @@ class TwostageDataReader(Dataset):
         atts_file_path = os.path.join(data_dir,atts_file_name)
 
         texts = file_read(text_file_path)
-        bios = file_read(bios_file_path)
-        atts = file_read(atts_file_path)
-
-        bios_labelid = bios_label_to_id(bios)
-
-        vocab_att_path = data_dir+'/vocab_attr_list.txt'
-        atts_labelid = atts_label_to_id(atts,vocab_att_path)
-
         bio_labels = []
-        for label in tqdm(bios_labelid,desc='bio_labels to tensor'):
-            label = torch.tensor(label,dtype=torch.long)
-            bio_labels.append(label)
-
         att_labels = []
-        for label in tqdm(atts_labelid, desc= 'att_labels to tensor'):
-            label = torch.tensor(label, dtype=torch.long)
-            att_labels.append(label)
+        if os.path.exists(bios_file_path) and os.path.exists(atts_file_path):
+            bios = file_read(bios_file_path)
+            atts = file_read(atts_file_path)
+
+            bios_labelid = bios_label_to_id(bios)
+            vocab_att_path = data_dir+'/vocab_attr_list.txt'
+            atts_labelid = atts_label_to_id(atts,vocab_att_path)
+
+            for label in tqdm(bios_labelid,desc='bio_labels to tensor'):
+                label = torch.tensor(label,dtype=torch.long)
+                bio_labels.append(label)
+
+            for label in tqdm(atts_labelid, desc= 'att_labels to tensor'):
+                label = torch.tensor(label, dtype=torch.long)
+                att_labels.append(label)
 
         input_ids_list = []
         attention_mask_list = []
@@ -50,10 +50,12 @@ class TwostageDataReader(Dataset):
             input_ids_list.append(input_ids)
             attention_mask_list.append(attention_mask)
 
-
-        for input_ids,attention_mask,bio_label,att_label in zip(input_ids_list,attention_mask_list,bio_labels,att_labels):
-            process_data_list.append((input_ids,attention_mask,bio_label,att_label))
-
+        if len(bio_labels)>0 and len(att_labels)>0:
+            for input_ids,attention_mask,bio_label,att_label in zip(input_ids_list,attention_mask_list,bio_labels,att_labels):
+                process_data_list.append((input_ids,attention_mask,bio_label,att_label))
+        else:
+            for input_ids,attention_mask in zip(input_ids_list,attention_mask_list):
+                process_data_list.append((input_ids,attention_mask,None,None))
         return process_data_list
 
 
